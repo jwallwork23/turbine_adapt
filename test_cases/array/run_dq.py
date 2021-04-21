d3ae44c3a766d7276b33ba19f81d00bb3cad36fe
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-04-21 10:58:06 +0100

    array: framework for goal-based adaptation

--- test_cases/array/run_dq.py
+++ test_cases/array/run_dq.py
@@ -4,27 +4,30 @@ import itertools
 from options import ArrayOptions
 
 
-end_time = 8928.0
-num_meshes = 80
+num_tidal_cycles = 0.125
+num_meshes = 5
+target = 40000.0
+norm_order = 1
+h_min = 0.1
+h_max = 1000.0
+maxiter = 0  # TODO
+element_rtol = 0.01
+qoi_rtol = 0.01
 
-# Set parameters
+# Mesh independent setup
 options = ArrayOptions(level=0)
-options.create_tidal_farm()
+end_time = num_tidal_cycles*options.tide_time
 output_dir = os.path.join(options.output_directory, 'fixed_mesh', 'level0')
 options.output_directory = create_directory(output_dir)
-
-# Create MixedFunctionSpace
-U_2d = VectorFunctionSpace(options.mesh2d, "DG", 1, name="U_2d")
-H_2d = get_functionspace(options.mesh2d, "DG", 1, name="H_2d")
-function_space = MixedFunctionSpace([U_2d, H_2d])
-
-# Turbine parametrisation
 Ct = options.quadratic_drag_coefficient
 ct = options.corrected_thrust_coefficient*Constant(pi/8)
-P0 = FunctionSpace(options.mesh2d, "DG", 0)
-for i, subdomain_id in enumerate(options.farm_ids):
-    subset = options.mesh2d.cell_subset(subdomain_id)
-    Ct = Ct + ct*interpolate(Constant(1.0), P0, subset=subset)
+dt = options.timestep
+timesteps = [dt]*num_meshes
+dt_per_export = int(options.simulation_export_time/dt)
+solves_per_dt = 1
+
+# Initial mesh sequence
+meshes = [Mesh(options.mesh2d.coordinates) for i in range(num_meshes)]
 
 
 def solver(ic, t_start, t_end, dt, J=0, qoi=None, recover_vorticity=False):
@@ -32,13 +35,15 @@ def solver(ic, t_start, t_end, dt, J=0, qoi=None, recover_vorticity=False):
     Solve forward over time window
     (`t_start`, `t_end`) in P1DG-P1DG space.
     """
+    mesh = ic.function_space().mesh()
+    options.create_tidal_farm(mesh=mesh)
+    P1 = get_functionspace(mesh, "CG", 1)
+    options.horizontal_viscosity = options.set_viscosity(P1)
     options.simulation_end_time = t_end
-    # if np.isclose(t_end, end_time):
-    #     options.simulation_end_time += 0.5*options.timestep
-    i_export = int(t_start/options.simulation_export_time)
+    i_export = int(np.round(t_start/options.simulation_export_time))
 
     # Create a new solver object and assign boundary conditions
-    solver_obj = FarmSolver(options)
+    solver_obj = FarmSolver(options, mesh=mesh)
     options.apply_boundary_conditions(solver_obj)
     options.J = J
     options.no_exports = True
@@ -61,24 +66,31 @@ def solver(ic, t_start, t_end, dt, J=0, qoi=None, recover_vorticity=False):
     solver_obj.simulation_time = t_start
     solver_obj.export_initial_state = False
 
+    # Turbine parametrisation
+    P0 = FunctionSpace(mesh, "DG", 0)
+    _Ct = Constant(Ct)
+    for i, subdomain_id in enumerate(options.farm_ids):
+        subset = mesh.cell_subset(subdomain_id)
+        _Ct = _Ct + ct*interpolate(Constant(1.0), P0, subset=subset)
+
     def update_forcings(t):
         options.update_forcings(t)
         if qoi is not None:
-            options.J += qoi(solver_obj.fields.solution_2d, t)
+            options.J += qoi(solver_obj.fields.solution_2d, t, turbine_drag=_Ct)
 
     # Solve forward on current subinterval
     solver_obj.iterate(update_forcings=update_forcings, export_func=options.export_func)
     return solver_obj.fields.solution_2d, options.J
 
 
-def time_integrated_qoi(sol, t):
+def time_integrated_qoi(sol, t, turbine_drag=Ct):
     """
     Power output of the array at time `t`.
 
     Integration in time gives the energy output.
     """
     u, eta = sol.split()
-    return Ct*pow(dot(u, u), 1.5)*dx
+    return turbine_drag*pow(dot(u, u), 1.5)*dx
 
 
 def initial_condition(fs):
@@ -96,73 +108,133 @@ def initial_condition(fs):
     return q
 
 
-# Setup
-J = 0
-fs = function_space
-dt = options.timestep
-dt_per_export = int(options.simulation_export_time/dt)
-solves_per_dt = 1
-qoi = time_integrated_qoi
-num_timesteps = int(end_time/dt)
-spaces = [fs]*num_meshes
+# Enter fixed point iteration
+converged = False
+converged_reason = None
+num_cells_old = None
+J_old = None
+for fp_iteration in range(maxiter+1):
+    if fp_iteration == maxiter:
+        converged = True
+        if converged_reason is None:
+            converged_reason = 'maximum number of iterations reached'
 
-# Solve forward and adjoint on each subinterval
-J, solutions = solve_adjoint(
-    solver, initial_condition, qoi, spaces, end_time, dt,
-    timesteps_per_export=dt_per_export, solves_per_timestep=solves_per_dt,
-)
+    # Create function spaces
+    spaces = [
+        MixedFunctionSpace([
+            VectorFunctionSpace(mesh, "DG", 1, name="U_2d"),
+            get_functionspace(mesh, "DG", 1, name="H_2d"),
+        ])
+        for mesh in meshes
+    ]
 
-# Plot solutions
-outdir = create_directory(os.path.join('outputs', 'fixed_mesh', f'{num_meshes}mesh'))
-fwd_outfile = File(os.path.join(outdir, 'Forward2d.pvd'))
-fwd_old_outfile = File(os.path.join(outdir, 'ForwardOld2d.pvd'))
-adj_outfile = File(os.path.join(outdir, 'Adjoint2d.pvd'))
-adj_next_outfile = File(os.path.join(outdir, 'AdjointNext2d.pvd'))
-err_outfile = File(os.path.join(outdir, 'Indicator2d.pvd'))
-difference_quotients = []
-for i in range(num_meshes):
+    # Solve forward and adjoint on each subinterval
+    J, solutions = solve_adjoint(
+        solver, initial_condition, time_integrated_qoi, spaces, end_time, dt,
+        timesteps_per_export=dt_per_export, solves_per_timestep=solves_per_dt,
+    )  # TODO: Option to just checkpoint only - use it if converged
 
-    # Create error estimator
-    ee = ErrorEstimator(options, mesh=options.mesh2d)  # NOTE: mesh will be passed in here
-    dq = Function(ee.P0, name="Difference quotient")
+    # Check for QoI convergence
+    if J_old is not None:
+        if abs(J - J_old) < qoi_rtol*J_old:
+            converged = True
+            converged_reason = 'converged quantity of interest'
 
-    N = len(solutions['adjoint'][i])
-    for j in range(N):
-        if i < num_meshes-1 and j == N-1:
-            continue
+    # # Escape if converged  # TODO
+    # if converged:
+    #     print_output(f"Termination due to {converged_reason}")
+    #     break
 
-        # Forward
-        u, eta = solutions['forward'][i][j].split()
-        u.rename("Velocity")
-        eta.rename("Elevation")
-        fwd_outfile.write(u, eta)
+    # Create vtu output files
+    outdir = create_directory(os.path.join('outputs', 'fixed_mesh', f'{num_meshes}mesh'))
+    outfiles = {
+        'forward': File(os.path.join(outdir, 'Forward2d.pvd')),
+        'forward_old': File(os.path.join(outdir, 'ForwardOld2d.pvd')),
+        'adjoint_next': File(os.path.join(outdir, 'AdjointNext2d.pvd')),
+        'adjoint': File(os.path.join(outdir, 'Adjoint2d.pvd')),
+    }
+    fields = ['forward', 'forward_old', 'adjoint_next', 'adjoint']
 
-        # Forward old
-        u_old, eta_old = solutions['forward_old'][i][j].split()
-        u_old.rename("Velocity")
-        eta_old.rename("Elevation")
-        fwd_old_outfile.write(u_old, eta_old)
+    # Loop over all meshes to evaluate error indicators
+    difference_quotients = []
+    for i, mesh in enumerate(meshes):
+        for f in fields:
+            outfiles[f]._topology = None  # Allow writing a different mesh
 
-        # Adjoint
-        z, zeta = solutions['adjoint'][i][j].split()
-        z.rename("Adjoint velocity")
-        zeta.rename("Adjoint elevation")
-        adj_outfile.write(z, zeta)
+        # Update FarmOptions object according to mesh
+        options.create_tidal_farm(mesh=mesh)
+        P1 = get_functionspace(mesh, "CG", 1)
+        options.horizontal_viscosity = options.set_viscosity(P1)
 
-        # Adjoint next
-        z_next, zeta_next = solutions['adjoint_next'][i][j].split()
-        z_next.rename("Adjoint velocity")
-        zeta_next.rename("Adjoint elevation")
-        adj_next_outfile.write(z_next, zeta_next)
+        # Create error estimator
+        ee = ErrorEstimator(options, mesh=mesh)
+        dq = Function(ee.P0, name="Difference quotient")
 
-        # Error indicator
-        _dq = ee.difference_quotient(u, eta, u_old, eta_old, z_next, zeta_next, z, zeta)
+        # Loop over all exported timesteps
+        N = len(solutions['adjoint'][i])
+        for j in range(N):
+            if i < num_meshes-1 and j == N-1:
+                continue
 
-        # Apply trapezium rule
-        if j in (0, N-1):
-            _dq *= 0.5
-        _dq *= dt
-        dq += _dq
-    difference_quotients.append(dq)
-for dq in reversed(difference_quotients):
-    err_outfile.write(dq)
+            # Plot fields
+            args = []
+            for f in fields:
+                args.extend(solutions[f][i][j].split())
+                args[-2].rename("Adjoint velocity" if 'adjoint' in f else "Velocity")
+                args[-1].rename("Adjoint elevation" if 'adjoint' in f else "Elevation")
+                outfiles[f].write(*args[-2:])
+
+            # Evaluate error indicator
+            _dq = ee.difference_quotient(*args)
+
+            # Apply trapezium rule
+            if j in (0, N-1):
+                _dq *= 0.5
+            _dq *= dt
+            dq += _dq
+        difference_quotients.append(dq)
+
+    # Plot difference quotient
+    outfiles['error'] = File(os.path.join(outdir, 'Indicator2d.pvd'))
+    for dq in reversed(difference_quotients):
+        outfiles['error']._topology = None
+        outfiles['error'].write(dq)
+
+    # Construct isotropic metrics
+    metrics = [
+        isotropic_metric(dq)
+        for dq in difference_quotients
+    ]
+    space_time_normalise(metrics, end_time, timesteps, target, norm_order)
+    enforce_element_constraints(metrics, h_min, h_max)
+
+    # Plot metrics
+    outfiles['metric'] = File(os.path.join(outdir, 'Metric2d.pvd'))
+    for metric in reversed(metrics):
+        metric.rename("Metric")
+        outfiles['metric']._topology = None
+        outfiles['metric'].write(metric)
+
+    # Adapt meshes
+    for i, metric in enumerate(metrics):
+        meshes[i] = Mesh(adapt(meshes[i], metric).coordinates)
+    num_cells = [mesh.num_cells() for mesh in meshes]
+
+    # Plot meshes
+    outfiles['mesh'] = File(os.path.join(outdir, 'Mesh2d.pvd'))
+    for mesh in reversed(meshes):
+        outfiles['mesh']._topology = None
+        outfiles['mesh'].write(mesh.coordinates)
+
+    # Check for convergence of element count
+    elements_converged = False
+    if num_cells_old is not None:
+        elements_converged = True
+        for nc, _nc in zip(num_cells, num_cells_old):
+            if abs(nc - _nc) > element_rtol*_nc:
+                elements_converged = False
+    num_cells_old = num_cells
+    if elements_converged:
+        print_output(f"Mesh element count converged to rtol {element_rtol}")
+        converged = True
+        converged_reason = 'converged element counts'
