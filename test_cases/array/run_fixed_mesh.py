9492aa6f0f010dd97bcafd3016f3838d3fe09c6c
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-05-25 17:11:41 +0100

    array: use VorticityCalculator2D

--- test_cases/array/run_fixed_mesh.py
+++ test_cases/array/run_fixed_mesh.py
@@ -21,18 +21,29 @@ parser.add_argument('-staggered', False, help="""
 parsed_args = parser.parse_args()
 
 # Set parameters
-ramp_dir = os.path.join('outputs', 'fixed_mesh', f'level{parsed_args.ramp_level}'))
-options = ArrayOptions(level=level, ramp_dir=ramp_dir, staggered=parsed_args.staggered)
+ramp_level = parsed_args.ramp_level
+ramp_dir = os.path.join('outputs', 'fixed_mesh', f'level{ramp_level}')
+options = ArrayOptions(
+    level=parsed_args.level, staggered=parsed_args.staggered,
+    ramp_dir=ramp_dir, ramp_level=ramp_level,
+)
 options.simulation_end_time = parsed_args.num_tidal_cycles*options.tide_time
 options.create_tidal_farm()
 output_dir = os.path.join(options.output_directory, 'fixed_mesh', f'level{parsed_args.level}')
 options.output_directory = create_directory(output_dir)
-options.recover_vorticity = True
 options.fields_to_export = ['uv_2d', 'elev_2d', 'vorticity_2d']
 
-# Solve
+# Create solver
 solver_obj = FarmSolver(options)
 options.apply_boundary_conditions(solver_obj)
 solver_obj.add_callback(PowerOutputCallback(solver_obj), 'timestep')
+vorticity_2d = Function(solver_obj.function_spaces.P1_2d, name='vorticity_2d')
+vorticity_calculator = VorticityCalculator2D(vorticity_2d, solver_obj)
+solver_obj.add_new_field(
+    vorticity_2d, 'vorticity_2d', 'Vorticity', 'Vorticity2d',
+    unit='s-1', preproc_func=vorticity_calculator.solve,
+)
 options.apply_initial_conditions(solver_obj)
+
+# Solve
 solver_obj.iterate(update_forcings=options.update_forcings, export_func=options.export_func)
