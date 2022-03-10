d8b43500ad082148154c53e298ce30e58a55e95b
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-03-10 15:46:37 +0000

    setup: reformat

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 
 setup(
-    name='turbine_adapt',
-    description='Mesh adaptation for tidal turbine problems',
-    author='Joseph Wallwork',
-    author_email='j.wallwork16@imperial.ac.uk',
-    url='https://github.com/jwallwork23/turbine_adapt',
-    version='0.1',
-    packages=['turbine', 'turbine_adapt'],
+    name="turbine_adapt",
+    description="Mesh adaptation for tidal turbine problems",
+    author="Joseph Wallwork",
+    author_email="j.wallwork16@imperial.ac.uk",
+    url="https://github.com/jwallwork23/turbine_adapt",
+    version="0.1",
+    packages=["turbine", "turbine_adapt"],
 )
