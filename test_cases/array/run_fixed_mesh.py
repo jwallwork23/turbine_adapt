867b8945f902fc9d5f8901fcfbd690e4c9f995cf
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-03-10 16:00:19 +0000

    array: direct solver option for run_fixed_mesh

--- test_cases/array/run_fixed_mesh.py
+++ test_cases/array/run_fixed_mesh.py
@@ -9,6 +9,16 @@ parser.add_argument(
     help="Name defining test case configuration",
     choices=["aligned", "staggered"],
 )
+parser.add_argument(
+    "--plot_vorticity",
+    help="Plot fluid vorticity",
+    action="store_true",
+)
+parser.add_argument(
+    "--use_direct_solver",
+    help="Apply a full LU decomposition",
+    action="store_true",
+)
 parser.parse_setup()
 parsed_args = parser.parse_args()
 config = parsed_args.configuration
@@ -35,18 +45,27 @@ options.output_directory = create_directory(output_dir)
 solver_obj = FarmSolver(options)
 options.apply_boundary_conditions(solver_obj)
 solver_obj.add_callback(PowerOutputCallback(solver_obj), "timestep")
-vorticity_2d = Function(solver_obj.function_spaces.P1_2d, name="vorticity_2d")
-uv_2d = solver_obj.fields.uv_2d
-vorticity_calculator = VorticityCalculator2D(uv_2d, vorticity_2d)
-solver_obj.add_new_field(
-    vorticity_2d,
-    "vorticity_2d",
-    "Vorticity",
-    "Vorticity2d",
-    unit="s-1",
-    preproc_func=vorticity_calculator.solve,
-)
-options.fields_to_export = ["uv_2d", "vorticity_2d"]
+if parsed_args.plot_vorticity:
+    vorticity_2d = Function(solver_obj.function_spaces.P1_2d, name="vorticity_2d")
+    uv_2d = solver_obj.fields.uv_2d
+    vorticity_calculator = VorticityCalculator2D(uv_2d, vorticity_2d)
+    solver_obj.add_new_field(
+        vorticity_2d,
+        "vorticity_2d",
+        "Vorticity",
+        "Vorticity2d",
+        unit="s-1",
+        preproc_func=vorticity_calculator.solve,
+    )
+    options.fields_to_export.append("vorticity_2d")
+if parsed_args.use_direct_solver:
+    options.swe_timestepper_options.solver_parameters = {
+        "mat_type": "aij",
+        "snes_type": "newtonls",
+        "ksp_type": "preonly",
+        "pc_type": "lu",
+        "pc_factor_mat_solver_type": "mumps",
+    }
 options.apply_initial_conditions(solver_obj)
 
 # Solve
