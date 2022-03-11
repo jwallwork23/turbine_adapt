fdc8335abdbe52df558d5199329184d9d0e34880
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-03-11 14:38:06 +0000

    array: allow plot_energy both ramp and run

--- test_cases/array/plot_energy.py
+++ test_cases/array/plot_energy.py
@@ -13,13 +13,15 @@ parser.add_argument(
     choices=["aligned", "staggered"],
 )
 parser.add_argument(
-    "--ramp",
-    help="Should we use ramp data, rather than subsequent run?",
-    action="store_true",
+    "mode",
+    help="Should we use ramp data and/or subsequent run?",
+    choices=["run", "ramp", "both"],
+    default="run",
 )
 parsed_args = parser.parse_args()
 config = parsed_args.configuration
-ramp = parsed_args.ramp
+mode = parsed_args.mode
+modes = ["ramp", "run"] if mode == "both" else [mode]
 
 
 def time_integrate(arr, dt=2.232):
@@ -36,29 +38,12 @@ def time_integrate(arr, dt=2.232):
     return dt * 0.5 * np.sum(off1 + off2, axis=0)
 
 
-# Setup directories
-if COMM_WORLD.size > 1:
-    print_output(
-        f"Will not attempt to plot with {COMM_WORLD.size} processors."
-        " Run again in serial."
-    )
-    sys.exit(0)
-pwd = os.path.dirname(__file__)
-plot_dir = create_directory(os.path.join(pwd, "plots", config))
-output_dir = os.path.join(pwd, "outputs", config)
-
 # Collect power/energy output data
 energy_output = {0: {}, 1: {}, 2: {}, 3: {}, 4: {}, "overall": {}}
-loops = {"fixed_mesh": range(5), "isotropic": 5000.0 * 2.0 ** np.array(range(6))}
+# loops = {"fixed_mesh": range(5), "isotropic": 5000.0 * 2.0 ** np.array(range(6))}
+loops = {"fixed_mesh": range(5)}
 for approach, levels in loops.items():
     for level in range(5):
-        fpath = f"{output_dir}/{approach}/level{level}"
-        if ramp:
-            fpath += "/ramp"
-        fname = f"{fpath}/diagnostic_turbine.hdf5"
-        if not os.path.exists(fname):
-            print_output(f"{fname} does not exist")
-            continue
         if approach == "fixed_mesh":
             options = ArrayOptions(level=level, staggered=config == "staggered")
             if options.element_family == "dg-dg":
@@ -67,20 +52,31 @@ for approach, levels in loops.items():
                 raise NotImplementedError
         else:
             raise NotImplementedError  # TODO: Read from log?
-        with h5py.File(fname, "r") as h5:
-            power = np.array(h5["current_power"]) * 1030.0 / 1.0e06  # MW
-            energy = time_integrate(power) / 3600.0  # MWh
-            for i in range(5):
-                if approach not in energy_output[i]:
-                    energy_output[i][approach] = {}
-                j = 3 * i
-                k = 3 * (i + 1)
-                energy_output[i][approach][dofs] = sum(energy[j:k])
-            if approach not in energy_output["overall"]:
-                energy_output["overall"][approach] = {}
-            energy_output["overall"][approach][dofs] = sum(energy)
+        output_dir = f"outputs/{config}/{approach}/level{level}"
+        power = np.array([]).reshape((0, 15))
+        for m in modes:
+            ramp = m == "ramp"
+            input_dir = output_dir + "/ramp" if ramp else output_dir
+            fname = f"{input_dir}/diagnostic_turbine.hdf5"
+            if not os.path.exists(fname):
+                print(f"{fname} does not exist")
+                continue
+            with h5py.File(fname, "r") as h5:
+                power = np.concatenate((power, np.array(h5["current_power"])))
+        power *= 1030.0 / 1.0e06  # MW
+        energy = time_integrate(power) / 3600.0  # MWh
+        for i in range(5):
+            if approach not in energy_output[i]:
+                energy_output[i][approach] = {}
+            j = 3 * i
+            k = 3 * (i + 1)
+            energy_output[i][approach][dofs] = sum(energy[j:k])
+        if approach not in energy_output["overall"]:
+            energy_output["overall"][approach] = {}
+        energy_output["overall"][approach][dofs] = sum(energy)
 
 # Plot DoF count vs energy output
+plot_dir = create_directory(f"plots/{config}")
 for subset, byapproach in energy_output.items():
     fig, axes = plt.subplots()
     for label, bydof in byapproach.items():
@@ -93,7 +89,4 @@ for subset, byapproach in energy_output.items():
     axes.grid(True)
     axes.legend()
     plt.tight_layout()
-    fname = f"{plot_dir}/energy_output_{subset}"
-    if ramp:
-        fname += "_ramp"
-    plt.savefig(fname + ".pdf")
+    plt.savefig(f"{plot_dir}/energy_output_{subset}_{mode}.pdf")
