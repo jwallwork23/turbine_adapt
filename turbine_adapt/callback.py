dfa65d3c8aba2d2932d7f8cb93f9c0d09169d138
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-03-11 10:44:18 +0000

    callback: power output array formatting

--- turbine_adapt/callback.py
+++ turbine_adapt/callback.py
@@ -10,15 +10,26 @@ class PowerOutputCallback(turbines.TurbineFunctionalCallback):
     print statements.
     """
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.shape = np.shape(self.solver_obj.options.array_ids)
+
     def message_str(self, current_power, average_power, average_profit):
-        power = 1030.0 * sum(current_power)
-        if power < 1.0e03:
-            return f"current power:     {power:5.3f} W"
-        elif power < 1.0e06:
-            return f"current power:     {power/1.0e+03:5.3f} kW"
-        elif power < 1.0e09:
-            return f"current power:     {power/1.0e+06:5.3f} MW"
-        elif power < 1.0e12:
-            return f"current power:     {power/1.0e+09:5.3f} GW"
-        else:
-            return f"current power:     {power:10.4e} W"
+        power = np.reshape(1030.0 * current_power, self.shape)
+        overall = np.sum(power.flatten())
+        unit = "W"
+        if overall < 1.0e3:
+            pass
+        elif overall < 1.0e06:
+            power /= 1.0e03
+            unit = "kW"
+        elif overall < 1.0e09:
+            power /= 1.0e06
+            unit = "MW"
+        elif overall < 1.0e12:
+            power /= 1.0e09
+            unit = "GW"
+        overall = np.sum(power.flatten())
+        colwise = ", ".join([f"{col:8.4f}" for col in np.sum(power, axis=0)])
+        unit = f"({unit})"
+        return f"power {unit:4s}: {overall:8.4f} [{colwise}]"
