fe95c640312d57caab0a44ad683516b120ccda97
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-03-11 14:23:39 +0000

    array: allow plot_power both ramp and run

--- test_cases/array/plot_power.py
+++ test_cases/array/plot_power.py
@@ -13,38 +13,45 @@ parser.add_argument(
     help="Name defining test case configuration",
     choices=["aligned", "staggered"],
 )
+parser.add_argument(
+    "mode",
+    help="Should we use ramp data and/or subsequent run?",
+    choices=["run", "ramp", "both"],
+    default="run",
+)
 parser.parse_setup()
 parser.parse_approach()
 parser.parse_metric_parameters()
-parser.add_argument(
-    "--ramp",
-    help="Should we use ramp data, rather than subsequent run?",
-    action="store_true",
-)
 parsed_args = parser.parse_args()
 config = parsed_args.configuration
 approach = parsed_args.approach
 level = parsed_args.level
 target = parsed_args.target_complexity
-ramp = parsed_args.ramp
+mode = parsed_args.mode
+modes = ["ramp", "run"] if mode == "both" else [mode]
 
-# Setup directories
 if approach == "fixed_mesh":
     run = f"level{level}"
 else:
     run = f"target{target:.0f}"
-input_dir = f"outputs/{config}/{approach}/{run}"
-if ramp:
-    input_dir += "/ramp"
+output_dir = f"outputs/{config}/{approach}/{run}"
 plot_dir = create_directory(f"plots/{config}/{approach}/{run}")
 
-# Load data
-with h5py.File(os.path.join(input_dir, "diagnostic_turbine.hdf5"), "r") as f:
-    power = np.array(f["current_power"])
-    time = np.array(f["time"])
-if not ramp:
-    time += 4464.0
-time /= 4464.0
+# Setup directories
+power = np.array([]).reshape((0, 15))
+time = np.array([]).reshape((0, 1))
+for m in modes:
+    ramp = m == "ramp"
+    input_dir = output_dir + "/ramp" if ramp else output_dir
+
+    # Load data
+    with h5py.File(os.path.join(input_dir, "diagnostic_turbine.hdf5"), "r") as f:
+        t = np.array(f["time"])
+        if not ramp:
+            t += 4464.0
+        t /= 4464.0
+        time = np.vstack((time, t))
+        power = np.vstack((power, np.array(f["current_power"])))
 
 colours = ["b", "C0", "mediumturquoise", "mediumseagreen", "g"]
 kw = {"linewidth": 1.0}
@@ -55,19 +62,17 @@ for i in range(15):
     _power = power[:, i] * 1030.0 / 1.0e06
     axes.plot(time, _power, label=f"Turbine {i+1}", color=colours[i // 3], **kw)
 axes.set_xlabel(r"Time/$T_{\mathrm{tide}}$")
-if ramp:
-    axes.set_xlim([0, 1])
+axes.set_xlim([time[0], time[-1]])
+if mode == "ramp":
     axes.set_xticks([0, 0.25, 0.5, 0.75, 1])
-else:
-    axes.set_xlim([1, 1.5])
+elif mode == "run":
     axes.set_xticks([1, 1.125, 1.25, 1.375, 1.5])
+else:
+    axes.set_xticks([0, 0.25, 0.5, 0.75, 1, 1.25, 1.5])
 axes.set_ylabel(r"Power output [$\mathrm{MW}$]")
 axes.grid(True)
 plt.tight_layout()
-fname = f"{plot_dir}/power_output"
-if ramp:
-    fname += "_ramp"
-plt.savefig(fname + ".pdf")
+plt.savefig(f"{plot_dir}/power_output_{mode}.pdf")
 
 # Plot power output of each column
 fig, axes = plt.subplots()
@@ -76,19 +81,17 @@ for i in range(5):
     axes.plot(time, _power * 1030.0 / 1.0e06, label=f"{i+1}", color=colours[i], **kw)
 axes.set_xlabel(r"Time/$T_{\mathrm{tide}}$")
 axes.set_ylabel(r"Power output [$\mathrm{MW}$]")
-if ramp:
-    axes.set_xlim([0, 1])
+axes.set_xlim([time[0], time[-1]])
+if mode == "ramp":
     axes.set_xticks([0, 0.25, 0.5, 0.75, 1])
-else:
-    axes.set_xlim([1, 1.5])
+elif mode == "run":
     axes.set_xticks([1, 1.125, 1.25, 1.375, 1.5])
+else:
+    axes.set_xticks([0, 0.25, 0.5, 0.75, 1, 1.25, 1.5])
 axes.set_ylim([0, 15])
 axes.grid(True)
 plt.tight_layout()
-fname = f"{plot_dir}/power_output_column"
-if ramp:
-    fname += "_ramp"
-plt.savefig(fname + ".pdf")
+plt.savefig(f"{plot_dir}/power_output_column_{mode}.pdf")
 
 # Plot legend separately
 fig2, axes2 = plt.subplots()
@@ -102,17 +105,15 @@ plt.savefig(f"{plot_dir}/legend_column.pdf", bbox_inches=bbox)
 # Plot total power output
 fig, axes = plt.subplots()
 axes.plot(time, np.sum(power, axis=1) * 1030.0 / 1.0e06, label=f"Turbine {i}", **kw)
-if ramp:
-    axes.set_xlim([0, 1])
+axes.set_xlim([time[0], time[-1]])
+if mode == "ramp":
     axes.set_xticks([0, 0.25, 0.5, 0.75, 1])
-else:
-    axes.set_xlim([1, 1.5])
+elif mode == "run":
     axes.set_xticks([1, 1.125, 1.25, 1.375, 1.5])
+else:
+    axes.set_xticks([0, 0.25, 0.5, 0.75, 1, 1.25, 1.5])
 axes.set_xlabel(r"Time [$\mathrm s$]")
 axes.set_ylabel(r"Power output [$\mathrm{MW}$]")
 axes.grid(True)
 plt.tight_layout()
-fname = f"{plot_dir}/total_power_output"
-if ramp:
-    fname += "_ramp"
-plt.savefig(fname + ".pdf")
+plt.savefig(f"{plot_dir}/total_power_output_{mode}.pdf")
