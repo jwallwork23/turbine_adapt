0a9b26928eb97706131571a9e4a1b52e268419a3
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-04-01 13:24:56 +0100

    array: tweaks to plot_dofs

--- test_cases/array/plot_dofs.py
+++ test_cases/array/plot_dofs.py
@@ -25,14 +25,19 @@ dofs = {}
 for config in ("aligned", "staggered"):
     cells = []
     for i in range(40):
-        mesh = meshio.read(f"outputs/{config}/{approach}/{run}/Mesh2d_{i}.vtu")
+        fname = f"outputs/{config}/{approach}/{run}/Mesh2d_{i}.vtu"
+        if not os.path.exists(fname):
+            print(f"File {fname} does not exist.")
+            break
+        mesh = meshio.read(fname)
         for cell_block in mesh.cells:
             if cell_block.type in ("triangle"):
                 num_cells = len(cell_block)
                 print(f"{i:2d}: {num_cells:6d} elements, {len(mesh.points):6d} vertices")
                 cells.append(num_cells)
                 continue
-    dofs[config] = 9 * np.array(cells)
+    if len(cells) == parsed_args.num_meshes:
+        dofs[config] = 9 * np.array(cells)
 x = np.array(range(num_meshes)) + 0.5
 ticks = np.arange(np.floor(x[0]), x[-1] + 1.0, 10)
 times = (options.ramp_time + ticks / ticks[-1] * options.simulation_end_time) / options.tide_time
@@ -40,8 +45,9 @@ ticklabels = [f"{t:.3f}" for t in times]
 
 # Plot DoF counts as a bar chart
 fig, axes = plt.subplots()
+n = len(dofs.keys())
 for i, (config, dof) in enumerate(dofs.items()):
-    axes.bar(x + i * 0.5, dof / 1000, width=0.5, label=config.capitalize(), color=f"C{2 * i}")
+    axes.bar(x + i / n, dof / 1000, width=1 / n, label=config.capitalize(), color=f"C{2 * i}")
 axes.set_xticks(ticks)
 axes.set_xticklabels(ticklabels)
 axes.set_xlim([ticks[0], ticks[-1]])
