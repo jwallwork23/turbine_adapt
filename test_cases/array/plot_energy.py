bb0e6edb717f0d3e7e2bdd22556a60442ba49101
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-04-04 09:19:11 +0100

    array: use get_data in plot_energy

--- test_cases/array/plot_energy.py
+++ test_cases/array/plot_energy.py
@@ -1,9 +1,6 @@
 from turbine_adapt import *
 from turbine_adapt.plotting import *
-from utils import time_integrate
-from options import ArrayOptions
-import h5py
-import numpy as np
+from utils import *
 
 
 # TODO: use utils.get_data
@@ -21,20 +18,23 @@ parser.add_argument(
     choices=["run", "ramp", "both"],
     default="run",
 )
+parser.parse_approach()
+parser.parse_metric_parameters()
 parser.parse_setup()
 parsed_args = parser.parse_args()
 config = parsed_args.configuration
 mode = parsed_args.mode
 modes = ["ramp", "run"] if mode == "both" else [mode]
-end_time = parsed_args.end_time
-num_tidal_cycles = parsed_args.num_tidal_cycles
 
 # Collect power/energy output data
 energy_output = {0: {}, 1: {}, 2: {}, 3: {}, 4: {}, "overall": {}}
 # loops = {"fixed_mesh": range(5), "isotropic": 5000.0 * 2.0 ** np.array(range(6))}
 loops = {"fixed_mesh": range(5)}
 for approach, levels in loops.items():
-    for level in range(5):
+    parsed_args.approach = approach
+    for level in levels:
+        parsed_args.level = level
+        # parsed_args.target_complexity =
         if approach == "fixed_mesh":
             options = ArrayOptions(level=level, configuration=config)
             if options.element_family == "dg-dg":
@@ -44,28 +44,8 @@ for approach, levels in loops.items():
         else:
             raise NotImplementedError  # TODO: Read from log?
         output_dir = f"outputs/{config}/{approach}/level{level}"
-        power = np.array([]).reshape((0, 15))
-        time = np.array([]).reshape((0, 1))
-        for m in modes:
-            if end_time is None:
-                end_time = 0.0
-                if m != "run":
-                    end_time += options.ramp_time
-                if m != "ramp":
-                    end_time += num_tidal_cycles * options.tide_time
-            input_dir = output_dir + "/ramp" if m == "ramp" else output_dir
-            fname = f"{input_dir}/diagnostic_turbine.hdf5"
-            if not os.path.exists(fname):
-                print(f"{fname} does not exist")
-                continue
-            with h5py.File(fname, "r") as h5:
-                power = np.concatenate((power, np.array(h5["current_power"])))
-                time = np.concatenate((time, np.array(h5["time"])))
-        if len(time.flatten()) == 0:
-            continue
-        power = power[time.flatten() <= end_time, :]
-        time = time[time <= end_time]
-        power *= 1030.0 / 1.0e06  # MW
+        power, time, energy, energy_time = get_data(config, modes, parsed_args)[:4]
+
         energy = time_integrate(power, time) / 3600.0  # MWh
         for i in range(5):
             if approach not in energy_output[i]:
