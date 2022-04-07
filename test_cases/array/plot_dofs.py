5fb2a9772d13a25aedc599dfe2fe941f853b0fdb
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-04-07 11:47:29 +0100

    array: plot_dofs columnwise

--- test_cases/array/plot_dofs.py
+++ test_cases/array/plot_dofs.py
@@ -6,26 +6,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Parse user input
-parser = Parser("test_cases/array/plot_dofs.py")
-parser.parse_setup()
-parser.parse_approach()
-parser.parse_metric_parameters()
-parsed_args = parser.parse_args()
-num_meshes = parsed_args.num_meshes
-approach = parsed_args.approach
-if approach == "fixed_mesh":
-    run = f"level{parsed_args.level}"
-else:
-    run = f"target{parsed_args.target_complexity:.0f}"
-options = ArrayOptions(meshgen=True)
-
-# Read cell data from VTU
-dofs = {}
-for config in ("aligned", "staggered"):
+def count_cells(fpath):
+    """
+    Look at the Mesh_*.vtu files in a given filepath
+    and count the cells in each.
+    """
     cells = []
     for i in range(40):
-        fname = f"outputs/{config}/{approach}/{run}/Mesh2d_{i}.vtu"
+        fname = f"{fpath}/Mesh2d_{i}.vtu"
         if not os.path.exists(fname):
             print(f"File {fname} does not exist.")
             break
@@ -36,29 +24,69 @@ for config in ("aligned", "staggered"):
                 print(f"{i:2d}: {num_cells:6d} elements, {len(mesh.points):6d} vertices")
                 cells.append(num_cells)
                 continue
-    if len(cells) == parsed_args.num_meshes:
+    return cells
+
+
+# Parse user input
+parser = Parser("test_cases/array/plot_dofs.py")
+parser.add_argument(
+    "--columns",
+    help="Compare turbine columns",
+    action="store_true",
+)
+parser.parse_setup()
+parser.parse_approach()
+parser.parse_metric_parameters()
+parsed_args = parser.parse_args()
+num_meshes = parsed_args.num_meshes
+approach = parsed_args.approach
+col_mode = parsed_args.columns
+if approach == "fixed_mesh":
+    run = f"level{parsed_args.level}"
+else:
+    run = f"target{parsed_args.target_complexity:.0f}"
+options = ArrayOptions(meshgen=True)
+
+# Read cell data from VTU
+dofs = {}
+if col_mode:
+    for ext in ("", "_2", "_4"):
+        fpath = f"outputs/staggered/{approach + ext}/{run}"
+        cells = count_cells(fpath)
+        dofs[approach + ext] = 9 * np.array(cells)
+else:
+    for config in ("aligned", "staggered"):
+        fpath = f"outputs/{config}/{approach}/{run}"
+        cells = count_cells(fpath)
         dofs[config] = 9 * np.array(cells)
+for val in dofs.values():
+    assert len(val) == parsed_args.num_meshes
 x = np.array(range(num_meshes)) + 0.5
 ticks = np.arange(np.floor(x[0]), x[-1] + 1.0, 10)
 times = (options.ramp_time + ticks / ticks[-1] * options.simulation_end_time) / options.tide_time
 ticklabels = [f"{t:.3f}" for t in times]
 
 # Print statistics
-for config, dof in dofs.items():
+for key, dof in dofs.items():
     min = np.min(dof)
     max = np.max(dof)
     mean = np.mean(dof)
     std = np.std(dof)
-    print(f"{config:9s}: min  {min/9:.0f} elements / {min:.0f} DoFs")
-    print(f"{config:9s}: max  {max/9:.0f} elements / {max:.0f} DoFs")
-    print(f"{config:9s}: mean {mean/9:.0f} elements / {mean:.0f} DoFs")
-    print(f"{config:9s}: std  {std/9:.0f} elements / {std:.0f} DoFs")
+    print(f"{key:9s}: min  {min/9:.0f} elements / {min:.0f} DoFs")
+    print(f"{key:9s}: max  {max/9:.0f} elements / {max:.0f} DoFs")
+    print(f"{key:9s}: mean {mean/9:.0f} elements / {mean:.0f} DoFs")
+    print(f"{key:9s}: std  {std/9:.0f} elements / {std:.0f} DoFs")
 
 # Plot DoF counts as a bar chart
 fig, axes = plt.subplots()
 n = len(dofs.keys())
-for i, (config, dof) in enumerate(dofs.items()):
-    axes.bar(x + i / n, dof / 1000, width=1 / n, label=config.capitalize(), color=f"C{2 * i}")
+for i, (key, dof) in enumerate(dofs.items()):
+    if col_mode:
+        c = key.split(approach)[-1]
+        label = "Whole array" if c == "" else f"Column {int(c[-1])+1}"
+    else:
+        label = key.capitalize()
+    axes.bar(x, dof / 1000, label=label, edgecolor=f"C{2 * i}", color="None")
 axes.set_xticks(ticks)
 axes.set_xticklabels(ticklabels)
 axes.set_xlim([ticks[0], ticks[-1]])
@@ -68,4 +96,7 @@ axes.set_ylabel(r"DoF count ($\times10^3$)")
 axes.legend(fontsize=16)
 axes.grid(True)
 plt.tight_layout()
-plt.savefig(f"plots/{approach}_{run}_dofs.pdf")
+if col_mode:
+    plt.savefig(f"plots/staggered_{approach}_{run}_dofs.pdf")
+else:
+    plt.savefig(f"plots/{approach}_{run}_dofs.pdf")
