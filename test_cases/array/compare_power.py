c958e9799c8011b6e7ca49c90a61355326fe5439
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-04-07 11:50:48 +0100

    array: fix compare_power and add energy computations

--- test_cases/array/compare_power.py
+++ test_cases/array/compare_power.py
@@ -7,7 +7,7 @@ import sys
 
 
 # Load data
-approaches = ("fixed_mesh", "isotropic")
+approaches = ("fixed_mesh", "isotropic_dwr")
 configs = ("aligned", "staggered")
 power = {}
 time = {}
@@ -27,18 +27,37 @@ for config in configs:
             P = np.array(f["current_power"])[t.flatten() <= end_time, :]
         power[config][approach] = P * 1030.0 / 1.0e06  # MW
         time[approach] = t[t <= end_time].flatten() / 3600.0  # h
-    assert np.allclose(time["fixed_mesh"], time["isotropic"])
+    assert np.allclose(time["fixed_mesh"], time["isotropic_dwr"])
 t = time["fixed_mesh"]
 nt = len(t)
+
+# Energy analysis
+for config in configs:
+    P = power[config]["fixed_mesh"].reshape(nt, 5, 3)
+    Ph = power[config]["isotropic_dwr"].reshape(nt, 5, 3)
+    E = time_integrate(np.sum(P, axis=2), t)
+    print(f"Fixed mesh energy for {config:10s}: {np.round(E, 2)}, {np.round(np.sum(E), 2)}")
+    Eh = time_integrate(np.sum(Ph, axis=2), t)
+    print(f"Adaptive energy for {config:10s}: {np.round(Eh, 2)}, {np.round(np.sum(Eh), 2)}")
+    err = np.round(100 * np.abs((E - Eh) / E), 1)
+    E = np.sum(E)
+    Eh = np.sum(Eh)
+    print(f"Relative energy error for {config:10s}: {err} %,"
+          f" {np.round(100 * np.abs((E - Eh) / E), 1)} %\n")
+
+
+def Lp_norm(arr, p=1):
+    return time_integrate(np.sum(np.abs(arr) ** p, axis=2), t) ** (1 / p)
+
 
 # Error analysis
 p = 1
 for config in configs:
     P = power[config]
-    err = np.abs(P["fixed_mesh"] - P["isotropic"]).reshape(nt, 3, 5)
-    Lp_err = time_integrate(np.sum(err**p, axis=0), t) ** (1 / p)
-    pwr = power[config]["fixed_mesh"].reshape(nt, 3, 5)
-    Lp_nrm = np.sqrt(time_integrate(np.sum(pwr**2, axis=0), t))
-    rel_Lp_err = np.round(100 * Lp_err / Lp_nrm, 1)
-    print(f"Relative L{p:.0f} error for {config:10s}: {rel_Lp_err} %,"
-          f" {np.round(np.sum(rel_Lp_err), 1)} %")
+    err = np.abs(P["fixed_mesh"] - P["isotropic_dwr"])
+    pwr = power[config]["fixed_mesh"]
+    rel_Lp_err = Lp_norm(err.reshape(nt, 5, 3)) / Lp_norm(pwr.reshape(nt, 5, 3))
+    rel_Lp_err = np.round(100 * rel_Lp_err, 1)
+    err = Lp_norm(err.reshape(nt, 1, 15)) / Lp_norm(pwr.reshape(nt, 1, 15))
+    err = np.round(100 * err, 1)
+    print(f"Relative L{p:.0f} error for {config:10s}: {rel_Lp_err} %, {err} %")
--- test_cases/array/utils.py
+++ test_cases/array/utils.py
@@ -15,7 +15,7 @@ def time_integrate(arr, times):
     """
     times = times.flatten()
     dt = times[1] - times[0]
-    zeros = np.zeros(arr.shape[1])
+    zeros = np.zeros(arr.shape[1:])
     off1 = np.vstack((zeros, arr))
     off2 = np.vstack((arr, zeros))
     return dt * 0.5 * np.sum(off1 + off2, axis=0)
