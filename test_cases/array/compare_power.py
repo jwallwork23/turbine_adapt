50f1478dc16da46fbae8d7ffaca0acbb4aaf0ca9
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-04-19 10:03:58 +0100

    array: formatting in compare_power

--- test_cases/array/compare_power.py
+++ test_cases/array/compare_power.py
@@ -56,14 +56,14 @@ nt = len(t)
 # Energy analysis
 P = power["fixed_mesh"].reshape(nt, 5, 3)
 E = time_integrate(np.sum(P, axis=2), t)
-print(f"fixed_mesh     / {config:10s} energy       {np.round(E, 2)}, {np.round(np.sum(E), 2)}")
+print(f"fixed_mesh     / {config:10s} energy            {np.round(E, 2)},  {np.round(np.sum(E), 2)}")
 head = f"{approach:15s}/ {config:10s}"
 Ph = power[approach].reshape(nt, 5, 3)
 Eh = time_integrate(np.sum(Ph, axis=2), t)
-print(f"{head} energy       {np.round(Eh, 2)}, {np.round(np.sum(Eh), 2)}")
+print(f"{head} energy            {np.round(Eh, 2)},  {np.round(np.sum(Eh), 2)}")
 err = np.round(100 * np.abs((E - Eh) / E), 1)
 overall = np.round(100 * np.abs((np.sum(E) - np.sum(Eh)) / np.sum(E)), 1)
-print(f"{head} energy error {err} %, {overall} %\n")
+print(f"{head} energy error      {err}%, {overall}%")
 
 
 def Lp_norm(arr, p=1):
@@ -79,7 +79,7 @@ rel_Lp_err = Lp_norm(err.reshape(nt, 5, 3)) / Lp_norm(pwr.reshape(nt, 5, 3))
 rel_Lp_err = np.round(100 * rel_Lp_err, 1)
 err = Lp_norm(err.reshape(nt, 1, 15)) / Lp_norm(pwr.reshape(nt, 1, 15))
 err = np.round(100 * err, 1)
-print(f"{head} relative L{p:.0f} error {rel_Lp_err} %, {err} %\n")
+print(f"{head} relative L{p:.0f} error {rel_Lp_err}%, {err}%")
 
 
 # Relative energy analysis
