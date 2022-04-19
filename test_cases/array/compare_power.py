1eb01eb5328051ae8e7c3adf93f16647d670396c
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-04-19 13:33:12 +0100

    array: use utils and uniform mesh for power comparison

--- test_cases/array/compare_power.py
+++ test_cases/array/compare_power.py
@@ -1,6 +1,6 @@
 from turbine_adapt.parse import Parser
-from utils import time_integrate
 from options import ArrayOptions
+from utils import *
 import h5py
 import numpy as np
 import os
@@ -22,7 +22,9 @@ parser.add_argument(
     help="Turbine columns to use in QoI",
     default=[0, 1, 2, 3, 4],
 )
+parser.parse_setup()
 parser.parse_approach()
+parser.parse_metric_parameters()
 parsed_args = parser.parse_args()
 config = parsed_args.configuration
 cols = np.sort([int(c) for c in parsed_args.columns])
@@ -30,33 +32,22 @@ ext = "".join([str(c) for c in cols])
 if ext != "01234":
     parsed_args.approach += "_" + ext
 approach = parsed_args.approach
-assert approach != "fixed_mesh"
+assert approach not in ("fixed_mesh", "uniform_mesh")
 
 # Load data
 power = {}
 time = {}
-for a in ("fixed_mesh", approach):
-    options = ArrayOptions(level=5, configuration=config, meshgen=True)
-    end_time = options.ramp_time + options.simulation_end_time
-    run = "level5" if a == "fixed_mesh" else "target10000"
-    output_dir = f"outputs/{config}/{a}/{run}"
-    fname = f"{output_dir}/diagnostic_turbine.hdf5"
-    if not os.path.exists(fname):
-        print(f"File {fname} does not exist")
-        sys.exit(0)
-    with h5py.File(fname, "r") as f:
-        t = np.array(f["time"]) + options.tide_time
-        P = np.array(f["current_power"])[t.flatten() <= end_time, :]
-    power[a] = P * 1030.0 / 1.0e06  # MW
-    time[a] = t[t <= end_time].flatten() / 3600.0  # h
-assert np.allclose(time["fixed_mesh"], time[approach])
-t = time["fixed_mesh"]
+for a in ("uniform_mesh", approach):
+    parsed_args["approach"] = a
+    power[a], time[a] = get_data(config, ["run"], parsed_args)[:2]
+assert np.allclose(time["uniform_mesh"], time[approach])
+t = time["uniform_mesh"]
 nt = len(t)
 
 # Energy analysis
-P = power["fixed_mesh"].reshape(nt, 5, 3)
+P = power["uniform_mesh"].reshape(nt, 5, 3)
 E = time_integrate(np.sum(P, axis=2), t)
-print(f"fixed_mesh     / {config:10s} energy            {np.round(E, 2)},  {np.round(np.sum(E), 2)}")
+print(f"uniform_mesh   / {config:10s} energy            {np.round(E, 2)},  {np.round(np.sum(E), 2)}")
 head = f"{approach:15s}/ {config:10s}"
 Ph = power[approach].reshape(nt, 5, 3)
 Eh = time_integrate(np.sum(Ph, axis=2), t)
@@ -72,7 +63,7 @@ def Lp_norm(arr, p=1):
 
 # Error analysis
 p = 1
-pwr = power["fixed_mesh"]
+pwr = power["uniform_mesh"]
 head = f"{approach:15s}/ {config:10s}"
 err = np.abs(pwr - power[approach])
 rel_Lp_err = Lp_norm(err.reshape(nt, 5, 3)) / Lp_norm(pwr.reshape(nt, 5, 3))
@@ -86,9 +77,9 @@ print(f"{head} relative L{p:.0f} error {rel_Lp_err}%, {err}%")
 tol = 0.1
 ones = np.ones((5, 3, 5))
 power[approach] /= np.clip(np.outer(outputs[config]["P"], ones).reshape(1000, 15), tol, np.Inf)
-P = power["fixed_mesh"].reshape(nt, 5, 3)
+P = power["uniform_mesh"].reshape(nt, 5, 3)
 E = time_integrate(np.sum(P, axis=2), t)
-print(f"fixed_mesh     / {config:10s} relative energy       {np.round(E, 2)}, {np.round(np.sum(E), 2)}")
+print(f"uniform_mesh   / {config:10s} relative energy       {np.round(E, 2)}, {np.round(np.sum(E), 2)}")
 head = f"{approach:15s}/ {config:10s}"
 Ph = power[approach].reshape(nt, 5, 3)
 Eh = time_integrate(np.sum(Ph, axis=2), t)
